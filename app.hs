20e02d2adbf6122ef86bda5b7d991c1b5833ca64
Maciej Michalec <maciej.michalec@gmail.com>
2021-04-13 20:54:53 +0200

    Extracted creating request to the separate method

--- app.hs
+++ app.hs
@@ -6,18 +6,19 @@ import Control.Monad.IO.Class
 import Data.Aeson
 import Network.HTTP.Req
 
+
+createRequest :: Req (JsonResponse Object)
+createRequest = let payload = object [ "foo" .= (10 :: Int), "bar" .= (20 :: Int)]
+                in req
+                   POST
+                   (https "httpbin.org" /: "post")
+                   (ReqBodyJson payload)
+                   jsonResponse
+                   mempty
+
+
 main :: IO ()
-main = runReq defaultHttpConfig $ do
-  let payload =
-        object
-          [ "foo" .= (10 :: Int),
-            "bar" .= (20 :: Int)
-          ]
-  r <-
-    req
-      POST -- method
-      (https "httpbin.org" /: "post")
-      (ReqBodyJson payload) 
-      jsonResponse
-      mempty
-  liftIO $ print (responseBody r :: Value)
+main = do
+  response <- runReq defaultHttpConfig createRequest
+  print $ responseBody response
+
