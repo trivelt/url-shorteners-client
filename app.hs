21ef1bcc1d730cc1ed17e96ed5e7913d7739cdb6
Maciej Michalec <maciej.michalec@gmail.com>
2021-04-13 21:02:54 +0200

    Added support for more APIs

--- app.hs
+++ app.hs
@@ -9,6 +9,39 @@ import Data.Text
 import Network.HTTP.Req
 
 
+
+shrtlnkDevRequest :: String -> Req (JsonResponse Object)
+shrtlnkDevRequest url = let payload = object [ "url" .= url ]
+                 in req
+                    POST
+                    (https "shrtlnk.dev" /: "api" /: "v2" /: "link")
+                    (ReqBodyJson payload)
+                    jsonResponse
+                    (header "api-key" "API_KEY")
+
+
+shrtlnkDevResponseHandler :: HttpResponseBody (JsonResponse Object) -> Text
+shrtlnkDevResponseHandler r = case HM.lookup "shrtlnk" r of
+                            Just (String url) -> url
+                            _ -> ""
+
+
+tlyRequest :: String -> Req (JsonResponse Object)
+tlyRequest url = let payload = object [ "long_url" .= url ]
+                 in req
+                    POST
+                    (https "t.ly" /: "api" /: "v1" /: "link" /: "shorten")
+                    (ReqBodyJson payload)
+                    jsonResponse
+                    mempty
+
+
+tlyResponseHandler :: HttpResponseBody (JsonResponse Object) -> Text
+tlyResponseHandler r = case HM.lookup "short_url" r of
+                            Just (String url) -> url
+                            _ -> ""
+
+
 tinyUIDRequest :: String -> Req (JsonResponse Object)
 tinyUIDRequest url = let payload = object [ "url" .= url ]
                 in req
